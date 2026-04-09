5759d5ddd23df450784f7ed491564f6e1b82b2f1
Barun <barun1234555@gmail.com>
2026-04-09 19:07:35 +0530

    commented temp code

--- week 5/COUNT_VOWELS.py
+++ week 5/COUNT_VOWELS.py
@@ -1,20 +1,10 @@
-# a = str(input("enter a string"))
-# b = ["a" , "e" , "i" , "o" , "u"]
-# count = 0
-# i = 0
-# for v in a:
-#     if(v in a):
-#         count = count+1
-# print(count)
-
-
 s = input("Enter a string: ") 
 
-vowels = "aeiouAEIOU" # Includes both small and capital letters
+vowels = "aeiouAEIOU" 
 count = 0 
 
 for ch in s:
-    if ch in vowels: # Check if the current character is a vowel
+    if ch in vowels: 
         count += 1 
 
 print("Number of vowels:", count)
